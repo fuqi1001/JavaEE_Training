70491f322270731f5c5162284806695b36645f7d
qifu <fuqiqi12345@gmail.com>
2017-11-09 23:03:00 -0500

    Facade design pattern

---
+++ Prepare/src/DesignPattern/Facade/FacadePatternDemo.java
@@ -0,0 +1,11 @@
+package DesignPattern.Facade;
+
+public class FacadePatternDemo {
+    public static void main(String[] args) {
+        ShapeMaker sm = new ShapeMaker();
+
+        sm.drawCircle();
+        sm.drawRectangle();
+        sm.drawSqure();
+    }
+}
